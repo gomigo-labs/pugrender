0b3d406393d6405a2de48f5b82e151d04946aa79
jaspreet <jaspreet@gomigo.io>
2022-02-18 11:18:30 +0530

    email template

--- server.js
+++ server.js
@@ -6,34 +6,18 @@ const moment = require("moment");
 const base64Img = require("base64-img");
 const { httpConstants } = require("./common/constants");
 
-// const dataJson = require("./data.json");
+const dataJson = require("./data/data.json");
 
 //variables
 let content;
 
-content = pug.renderFile("sample.pug", {
-  // title: invoiceJson.invoiceNumber,
-  // GSTIN: invoiceJson.customerGSTIN,
-  // organizationNo: invoiceJson.organizationNo || " ",
-  // invoiceNumber: invoiceJson.invoiceNumber,
+content = pug.renderFile("./templates/sample.pug", {
+  data: dataJson,
   // invoiceDate: moment(invoiceJson.invoiceDate).format("DD/MM/YYYY"),
   // dueDate: moment(invoiceJson.dueDate).format("DD/MM/YYYY"),
-  // lineItems: invoiceJson.lineItems,
-  // taxableAmount: invoiceJson.taxableAmount,
-  // invoiceQr: invoiceJson.invoiceQr,
-  // orgJson: orgJsonData,
-  // invoiceJsonData: invoiceJson,
   // uniqueTaxes: Object.entries(invoiceJson.uniqueTaxes),
   // debitNoteUniqueTaxes: Object.entries(debitNoteJson.uniqueTaxes),
   // creditNoteUniqueTaxes: Object.entries(creditNoteJson.uniqueTaxes),
-  // pharmaInvoice: pharmaInvoiceJson,
-  // lineItemsPharma: pharmaInvoiceJson.lineItems,
-  // time: moment().format("HH:MM"),
-  // hsnData: hsnData,
-  // debitNoteJson: debitNoteJson,
-  // debitNoteLineItems: debitNoteJson.lineItems,
-  // creditNoteJson: creditNoteJson,
-  // salesOrderJson: salesOrderJson,
 });
 
 fs.writeFile("sample_html.html", content, function (err, data) {
